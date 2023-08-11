9c4e7c3201863020c51b7040bf371648677115b4
Deeya S Doshi <65722056+deeyasdoshi@users.noreply.github.com>
2023-08-10 22:26:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,109 +1,134 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const expressSession = require('express-session')
+const express = require('express');
+const app = express(); // creates express app application
+const path = require('path');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const ejs = require('ejs');
+const expressSession = require('express-session');
 const bodyParser = require('body-parser')
 
-//In-built Middlewares
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.static('public'))
-app.set('view engine','ejs')
-app.use(bodyParser.json())
-app.use(expressSession({
-    secret:'I love fullstack'
-}))
 
-//Custom Middlewares
-const adminAuthMiddleware = require('./middlewares/adminAuthMiddleware')
-const authMiddleware = require('./middlewares/authMiddleware')
-const redirectIfAuthenticated = require('./middlewares/redirectIfAuthenticated')
-
-//for navbar control
-global.loggedIn=null
-app.use('*',(req,res,next)=>{
-        loggedIn = req.session.userId
-        next()
-})
-
-global.userType = null
-app.use('*',(req,res,next)=>{
-    userType = req.session.userType
-    next()
-})
-
-//Controllers
-const homeController = require('./controllers/homeController')
-const loginController = require('./controllers/loginController')
-const g2_test_controller = require('./controllers/g2_test_controller')
-const g_test_controller = require('./controllers/g_test_controller')
-const signUp_controller = require('./controllers/signUp_controller')
-const g2_data_store_controller = require('./controllers/g2_data_store_controller')
-const display_details_controller = require('./controllers/display_details_controller')
-const edit_details_controller = require('./controllers/edit_details_controller')
-const update_car_details_controller = require('./controllers/update_car_details_controller')
-const store_user_controller = require("./controllers/store_user_controller")
-const loginUserontroller = require("./controllers/loginUser")
-const logoutController = require('./controllers/logoutController')
-const appointment = require('./controllers/appointments')
-const sendAppointmentData = require('./controllers/sendAppointmentData.js')
+//Controller files
+const homeController = require('./controllers/homeController');
+const loginController = require('./controllers/loginController');
+const g2testController = require('./controllers/g2testController');
+const getg2testController = require('./controllers/getg2controller');
+const storeg2testController = require('./controllers/storeg2Controller');
+const gtestController = require('./controllers/gtestController');
+const displaygtestController = require('./controllers/displaygtestController');
+const updateController = require('./controllers/updateController');
+const storeupdateController = require('./controllers/storeupdateController');
+const signupController = require('./controllers/signupController');
+const storeuseraftersignupController = require('./controllers/storeuseraftersignupController');
+const loginuserController = require('./controllers/loginuserController');
+const logoutController = require('./controllers/logoutController');
+const appointment = require('./controllers/appointments');
+const sendAppointmentDataController = require('./controllers/appointmenteditController');
 const matchDate = require('./controllers/matchDate.js') 
 const fetchAppointmentData = require('./controllers/fetchAppointmentData.js')
-const updateTimeSlots = require('./controllers/updateTimeSlots.js')
+const updateTimeSlotsG2 = require('./controllers/updateTimeSlotsG2.js')
+const updateTimeSlotsG = require('./controllers/updateTimeSlotsG.js')
+const examinerController = require('./controllers/examinerController');
 
-app.listen(5000,()=>{
-    console.log('Hi I am listening at 5000')
+//custom middlewares
+const authMiddleware = require('./middleware/authMiddleware');
+const redirectifauthenticatedMiddleware = require('./middleware/redirectifauthenticated');
+const auth2Middleware = require('./middleware/auth2Middleware');
+const auth3Middleware = require('./middleware/auth3Middleware');
+
+//in-built middlewares
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(express.json());  //we mention the middlewares using app.use function
+app.use(express.urlencoded());  //we mention the middlewares using app.use function
+app.use(expressSession({
+    secret : 'love express'
+}))
+
+//for navbar control
+global.loggedIn = null;
+// value of req.session is found we assign it to loggedIn variable
+app.use("*" , (req,res,next) => {
+    loggedIn = req.session.userId;
+    usertype = req.session.usertype;
+    next();
 })
 
-//Route 1 : dashboard
-app.get('/',homeController)
 
-//Route 2 : login
-app.get('/login',loginController)
 
-//Route 3 : g2_test
-app.get('/g2_test',authMiddleware,g2_test_controller)
+app.listen(5000, ()=>{
+    console.log("App is listening on port 5000");
+})
 
-//Route 4 : g_test
-app.get('/g_test',authMiddleware,g_test_controller)
+// Database connection
+mongoose.connect('mongodb+srv://admin1:adminone@cluster0.njpmf30.mongodb.net/drivepost?retryWrites=true&w=majority');
 
-//Route 5: signUp
-app.get('/signUp',signUp_controller)
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.g4lqotk.mongodb.net/G2Data?retryWrites=true&w=majority')
+//Route 1 - Home page
+app.get('/', homeController );
 
-//Route 6: stores data entered on G2 page
-app.post("/g2_test/store",g2_data_store_controller)
+//Route 2 - Login page
+app.get('/login', redirectifauthenticatedMiddleware , loginController );
 
-//Route 7: displays the data on G2 page from G2 edit button apears
-app.post('/g_test/display_details',display_details_controller)
+//Route 3 - after entering login details in login page
+app.post('/users/login' , redirectifauthenticatedMiddleware , loginuserController );
 
-//Route 8: editable page opens and allows to edit car details
-app.post('/edit_details/:license',edit_details_controller)
+//Route 4 - Signup Page
+app.get('/auth/signup' , redirectifauthenticatedMiddleware , signupController );
 
-//Route 9: responsible for updating the car details in databse
-app.post('/edit_details_update/:license',update_car_details_controller)
+//Route 5 After signing up in signup page
+app.post('/users/signup' , redirectifauthenticatedMiddleware , storeuseraftersignupController );
 
-//Route 10: stores the user details from sign up page 
-app.post('/user/signUp',redirectIfAuthenticated,store_user_controller)
+//Route 6 - G2_test page
+app.get('/g2test', authMiddleware, g2testController );
 
-//Route 11: takes data from user and valdates it
-app.post('/user/login',redirectIfAuthenticated,loginUserontroller)
+//Route 7 - 
+app.get('/g2test/new', authMiddleware , getg2testController );
 
-//Route 12: logout controller
-app.get('/auth/logout',logoutController)
+//Route 8 - 
+app.post('/g2test/store', authMiddleware , storeg2testController );
 
-//Route 13: select a date
-app.get('/appointment/selectedDate',adminAuthMiddleware,matchDate)
+//Route 9 - G_test page
+app.get('/gtest', authMiddleware, gtestController );
 
-//Route 14: appointments
-app.post('/appointment/fetched_slots',adminAuthMiddleware,appointment)
+//Route 10 - displays user details in G_test page
+app.post('/gtest/display', displaygtestController );
 
-//Route 15: send data to database
-app.post('/appointmentBooked',sendAppointmentData)
+//Route 11 - allows to edit details of user in G_test page
+app.post('/display/edit/:license', updateController );
 
-//Route 16: fetch data from appointments 
+//Route 12 - updates user details after editting in G_test page
+app.post('/display/edit/update/:license' , storeupdateController );
+
+//Route 13 - Logout page
+app.get('/auth/logout' , logoutController);
+
+//Route 14: select a date
+app.get('/appointment/selectedDate',auth2Middleware,matchDate)
+
+//Route 15: appointments
+app.post('/appointment/fetched_slots',auth2Middleware,appointment)
+
+//Route 16: send data to database
+app.post('/appointmentBooked', sendAppointmentDataController)
+
+//Route 17: fetch data from appointments  in g2_test page
 app.get("/g2_test/data", fetchAppointmentData);
 
-//Route 17: update data in appointment collection
-app.post("/g2_test/appointmentBooked", updateTimeSlots)
+//Route 18: fetch data from appointments in g_test page
+app.get("/g_test/data", fetchAppointmentData);
+
+//Route 19: update data in appointment collection in g2_test page
+app.post("/g2_test/appointmentBooked", updateTimeSlotsG2)
+
+//Route 20: update data in appointment collection in g_test page
+app.post("/g_test/appointmentBooked", updateTimeSlotsG)
+
+//Route 21: Examiner page
+app.get('/examiner' , auth3Middleware, examinerController);
+
+
+
+
+
